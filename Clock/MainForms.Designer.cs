2a1b545ae003573fbcff7b774f2cad939c8495db
Roman Yarkov <roman121998yarkov@gmail.com>
2024-09-29 13:26:54 +0400

    Add project files.

---
+++ Clock/MainForms.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Clock
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.LabelTime = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// LabelTime
+			// 
+			this.LabelTime.AutoSize = true;
+			this.LabelTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.LabelTime.Location = new System.Drawing.Point(115, 0);
+			this.LabelTime.Name = "LabelTime";
+			this.LabelTime.Size = new System.Drawing.Size(174, 73);
+			this.LabelTime.TabIndex = 0;
+			this.LabelTime.Text = "Time";
+			this.LabelTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.ClientSize = new System.Drawing.Size(404, 284);
+			this.ControlBox = false;
+			this.Controls.Add(this.LabelTime);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "MainForm";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label LabelTime;
+	}
+}
+
